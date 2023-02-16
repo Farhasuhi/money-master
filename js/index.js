83447b372e495169280d34f53db8ff126f96fb1a
Farhasuhi <suhifarhaislam@gmail.com>
2023-02-17 00:26:09 +0600

    d

---
+++ js/index.js
@@ -0,0 +1,60 @@
+document.getElementById('calculate').addEventListener('click',function(){
+    const incomeValue=getInput('income');
+    const foodValue=getInput('food');
+    const rentValue=getInput('rent');
+    const clothsValue=getInput('cloths');
+    
+    if((isNaN(incomeValue))||(isNaN(foodValue))||(isNaN(rentValue))||(isNaN(clothsValue))){
+        alert("Please provide only number");
+        return;
+    }
+    if((incomeValue<0)||(foodValue<0)||(rentValue<0)||(clothsValue<0)){
+        alert("You cannot put negative number.Please provide only positive number");
+        return;
+    }
+
+    const totalExpense=clothsValue+foodValue+rentValue;
+    const balance=incomeValue-totalExpense;
+
+    if(totalExpense>incomeValue){
+        alert("our expense more than your income ");
+        return;
+    }
+
+   innerText('total-expense',totalExpense);
+   innerText('balance',balance);
+
+   
+
+});
+document.getElementById('save').addEventListener('click',function(){
+    const balanceInput=document.getElementById('balance');
+    const balanceString=balanceInput.innerText;
+    const balance=parseInt(balanceString);
+
+    const save=(balance*.2);
+    const saveFloat=save.toFixed(2);
+    const total=parseFloat(save);
+
+    const saveInputField=document.getElementById('save-input');
+    saveInputField.value=saveFloat;
+    
+    const remainingBalance=balance-saveFloat;
+
+    innerText('save-amount',total);
+    innerText('remaining-balance',remainingBalance);
+    
+
+})
+
+function getInput(id){
+    const inputField=document.getElementById(id);
+    const inputString=inputField.value;
+    const input=parseInt(inputString);
+    return input;
+}
+
+function innerText(id,value){
+    const text=document.getElementById(id);
+    text.innerText=value;
+}
